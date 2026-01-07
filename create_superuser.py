47a16deb9cc015a9e5f864644dc6c3af49ebe115
ranjit-design <ranjitchaudhary057@gmail.com>
2026-01-07 21:42:28 +0545

    Finally Updated

---
+++ create_superuser.py
@@ -0,0 +1,14 @@
+# create_superuser.py
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rs1500_backend.settings')
+django.setup()
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'password')
+    print("Superuser created!")
+else:
+    print("Superuser already exists.")
